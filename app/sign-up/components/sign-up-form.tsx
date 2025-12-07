5e447459453afb08494ede2769ce8696154d195d
dumure <resulpro291280@gmail.com>
2025-12-07 19:29:58 +0400

    sign up task

--- app/sign-up/components/sign-up-form.tsx
+++ app/sign-up/components/sign-up-form.tsx
@@ -1,13 +1,12 @@
 import Button from "@/components/ui/button";
 import SosialButton from "@/components/ui/sosial-button";
 import { layoutTheme } from "@/constant/theme";
-import { useAuthStore } from "@/store/auth.store";
 import { zodResolver } from "@hookform/resolvers/zod";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Label } from "@react-navigation/elements";
 import { Link, useRouter } from "expo-router";
 import { Controller, useForm } from "react-hook-form";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Alert, Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View } from "react-native";
 import UUID from "react-native-uuid";
 import { signUpSchema, SignUpSchema } from "./sign-up.schema";
 
@@ -23,6 +22,7 @@ export default function SignUpForm() {
     defaultValues: {
       email: "",
       password: "",
+      name: "",
     },
   });
 
@@ -34,120 +34,131 @@ export default function SignUpForm() {
       password: data.password,
       isAuthenticated: false,
     };
-    console.log(userData);
 
     try {
       await AsyncStorage.setItem("user", JSON.stringify(userData));
       router.replace("/sign-in/page");
     } catch (e) {
       console.log("Error saving user", e);
+      Alert.alert("Error", "An error occurred while saving your data. Please try again.");
     }
   };
 
   return (
-    <View style={styles.container}>
-      {/* Header */}
-      <View style={styles.header}>
-        <View style={styles.headerContent}>
-          <Text style={styles.title}>Sign Up</Text>
-          <View style={styles.underline} />
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <View style={styles.container}>
+          {/* Header */}
+          <View style={styles.header}>
+            <View style={styles.headerContent}>
+              <Text style={styles.title}>Sign Up</Text>
+              <View style={styles.underline} />
+            </View>
+          </View>
+
+          {/* FORM GROUP */}
+          <View style={styles.formGroup}>
+            <Label style={styles.label}>FULL NAME</Label>
+            <Controller
+              control={control}
+              name="name"
+              render={({ field: { value, onChange, onBlur } }) => (
+                <>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Name"
+                    value={value}
+                    onChangeText={onChange}
+                    onBlur={onBlur}
+                  />
+                  {errors.name && (
+                    <Text style={styles.error}>{errors.name?.message}</Text>
+                  )}
+                </>
+              )}
+            />
+          </View>
+
+          <View style={styles.formGroup}>
+            <Label style={styles.label}>EMAIL OR PHONE</Label>
+            <Controller
+              control={control}
+              name="email"
+              render={({ field: { value, onChange, onBlur } }) => (
+                <>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Email"
+                    value={value}
+                    onChangeText={onChange}
+                    onBlur={onBlur}
+                  />
+                  {errors.email && (
+                    <Text style={styles.error}>{errors.email.message}</Text>
+                  )}
+                </>
+              )}
+            />
+          </View>
+
+          <View style={styles.formGroup}>
+            <Label style={styles.label}>PASSWORD</Label>
+            <Controller
+              control={control}
+              name="password"
+              render={({ field: { value, onChange, onBlur } }) => (
+                <>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Password"
+                    value={value}
+                    onChangeText={onChange}
+                    onBlur={onBlur}
+                    secureTextEntry
+                  />
+                  {errors.password && (
+                    <Text style={styles.error}>{errors.password?.message}</Text>
+                  )}
+                </>
+              )}
+            />
+          </View>
+
+          {/* BUTTONS */}
+          <View style={styles.buttonsContainer}>
+            <Button title="Sign Up" onPress={handleSubmit(onSubmit)} />
+            <Text style={styles.orText}>OR</Text>
+            <SosialButton
+              icon={require("../../../assets/images/icons/google-icon.png")}
+              title="Sign Up with Google"
+              onPress={() => {}}
+            />
+            <SosialButton
+              icon={require("../../../assets/images/icons/facebook-icon.png")}
+              title="Sign Up with Facebook"
+              onPress={() => {}}
+            />
+          </View>
+
+          {/* Sign In Link */}
+          <View style={styles.linkContainer}>
+            <Text style={styles.linkText}>Already have an account? </Text>
+            <Link href="/sign-in/page" style={styles.link}>
+              Sign In
+            </Link>
+          </View>
         </View>
-      </View>
-
-      {/* FORM GROUP */}
-      <View style={styles.formGroup}>
-        <Label style={styles.label}>FULL NAME</Label>
-        <Controller
-          control={control}
-          name="name"
-          render={({ field: { value, onChange, onBlur } }) => (
-            <>
-              <TextInput
-                style={styles.input}
-                placeholder="Name"
-                value={value}
-                onChangeText={onChange}
-                onBlur={onBlur}
-              />
-              {errors.name && (
-                <Text style={styles.error}>{errors.name?.message}</Text>
-              )}
-            </>
-          )}
-        />
-      </View>
-      <View style={styles.formGroup}>
-        <Label style={styles.label}>EMAIL OR PHONE</Label>
-        <Controller
-          control={control}
-          name="email"
-          render={({ field: { value, onChange, onBlur } }) => (
-            <>
-              <TextInput
-                style={styles.input}
-                placeholder="Email"
-                value={value}
-                onChangeText={onChange}
-                onBlur={onBlur}
-              />
-              {errors.email && (
-                <Text style={styles.error}>{errors.email.message}</Text>
-              )}
-            </>
-          )}
-        />
-      </View>
-      <View style={styles.formGroup}>
-        <Label style={styles.label}>PASSWORD</Label>
-        <Controller
-          control={control}
-          name="password"
-          render={({ field: { value, onChange, onBlur } }) => (
-            <>
-              <TextInput
-                style={styles.input}
-                placeholder="Password"
-                value={value}
-                onChangeText={onChange}
-                onBlur={onBlur}
-                secureTextEntry
-              />
-              {errors.password && (
-                <Text style={styles.error}>{errors.password?.message}</Text>
-              )}
-            </>
-          )}
-        />
-      </View>
-
-      {/* BUTTONS */}
-      <View style={styles.buttonsContainer}>
-        <Button title="Sign Up" onPress={handleSubmit(onSubmit)} />
-        <Text style={styles.orText}>OR</Text>
-        <SosialButton
-          icon={require("../../../assets/images/icons/google-icon.png")}
-          title="Sign Up with Google"
-          onPress={() => {}}
-        />
-        <SosialButton
-          icon={require("../../../assets/images/icons/facebook-icon.png")}
-          title="Sign Up with Google"
-          onPress={() => {}}
-        />
-      </View>
-
-      <View style={styles.linkContainer}>
-        <Text style={styles.linkText}>Already have an account? </Text>
-        <Link href="/sign-in/page" style={styles.link}>
-          Sign In
-        </Link>
-      </View>
-    </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     padding: 20,
     gap: 20,
   },
